f4bee621213da7144e3e6155e891a1a1bfcdc584
raulhess <raul.hess@gmail.com>
2020-08-31 00:47:16 -0300

    criado componente tabela com dados mocados

---
+++ src/components/Tabela.vue
@@ -0,0 +1,44 @@
+<template>
+  <table :style="style">
+    <th v-for="(col,colIndex) of colunas" :key="'th-'+colIndex">
+      {{col.label}}
+    </th>
+    <tr v-for="(data,dataIndex) of dados" :key="'tr-'+dataIndex">
+      <td v-for="(col,colIndex) of colunas" :key="'td-'+colIndex">
+        {{getCol(data,col.nome)}}
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    colunas: {
+      type: Array,
+      default: () => [],
+    },
+    dados: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  computed: {
+    style() {
+      return ['width: 100%'];
+    },
+  },
+
+  methods: {
+    getCol(data, colName) {
+      if (data[colName]) return data[colName];
+      return '';
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/pages/Lista.vue
+++ src/pages/Lista.vue
@@ -1,10 +1,28 @@
 <template>
-  <div></div>
+  <div >
+    <tabela :colunas="colunas" :dados="dados" />
+  </div>
 </template>
 
 <script>
-export default {
+import Tabela from 'components/Tabela';
 
+export default {
+  data() {
+    return {
+      colunas: [
+        { nome: 'nome', label: 'Nome' },
+        { nome: 'email', label: 'Email' },
+      ],
+      dados: [
+        { nome: 'Raul Correa Hess', email: 'raul.hess@gmail.com' },
+        { nome: 'Carlos Alberto Hess', email: 'carlos.hess@gmail.com' },
+      ],
+    };
+  },
+  components: {
+    Tabela,
+  },
 };
 </script>
 
